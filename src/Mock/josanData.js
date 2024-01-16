ae22c5e69007a2fb658ba87ad655ccbbd72550dd
santosh yaduvanshi <santoshbishwas99@gmail.com>
2024-01-16 23:49:13 +0530

    first commit

--- src/Mock/josanData.js
+++ src/Mock/josanData.js
@@ -3,74 +3,86 @@ export const WebProduct = [
      " course_id": 1,
       "image_url": "https://assets.zyrosite.com/Aq20eV79zLfpXV6b/web_developing-dJoGejvV7nI9KNlO.gif",
       "course_name": "Web Development",
-      "course_details": "Web development internship that provides practical work experience and an introduction to crafting and enhancing web-based systems. This opportunity offers engaging challenges and real-world projects, allowing you to gain hands-on experience in the dynamic fields of web and app development. Join our award-winning innovation team and kickstart your journey to success in a supportive and enriching environments."
+      "course_details": "this program provides practical work experience and   crafting and enhancing web-based systems, This opportunity offers engaging challenges and real-world projects"
+      // This opportunity offers engaging challenges and real-world projects, allowing you to gain hands-on experience in the dynamic fields of web and app development. Join our award-winning innovation team and kickstart your journey to success in a supportive and enriching environments.
     },
     {
         "course_id": 2,
         "image_url": "https://cdna.artstation.com/p/assets/images/images/051/677/778/large/alexey-1.jpg?1657889349",
         "course_name": "Python",
-        "course_details": "Join our comprehensive internship program and master the fundamentals of programming in Python from the comfort of your own home. Gain the skills and knowledge to apply for exciting job opportunities in the field. In this program, you will learn everything from web development to the deployment of Python-based web applications. Get certified and enhance your career prospects. Don't miss this opportunity to excel in Python programming!"
+        "course_details": "Join our comprehensive internship program and master the fundamentals of programming in Python from the comfort of your own home, certified and enhance your career prospects.."
+        //  Gain the skills and knowledge to apply for exciting job opportunities in the field. In this program, you will learn everything from web development to the deployment of Python-based web applications. Get certified and enhance your career prospects. Don't miss this opportunity to excel in Python programming!//
       },
       {
         "course_id": 3,
         "image_url": "https://assets.zyrosite.com/Aq20eV79zLfpXV6b/mob-dev-AGBrMO8XElc08jln.gif",
         "course_name": "Mobile App Development",
-        "course_details": "Android, the user-friendly open-source operating system, has transformed the way we access internet applications and carry out important tasks on our mobile deviceBISHWAS TECHNOLOGY, we understand the growing preference for mobile usage and offer the ideal starting point for your app development journey. Discover the simplicity of creating your first app with us and unlock a world of endless possibilities in the realm of mobile innovation."
+        "course_details": "Android, the user-friendly open-source operating system, has transformed the way we access internet applications and carry out important tasks on our mobile device"
+      //.BISHWAS TECHNOLOGY, we understand the growing preference for mobile usage and offer the ideal starting point for your app development journey. Discover the simplicity of creating your first app with us and unlock a world of endless possibilities in the realm of mobile innovation.//
       },
       {
         "course_id": 4,
         "image_url": "	https://img.etimg.com/thumb/msid-20911876,width-650,height-488,imgsize-26758,resizemode-75/.jpg",
         "course_name": "Robotics",
-        "course_details": "Upon enrollment into the Robotics  Classes for kids, you will get your child's Robotics Learning kit at your doorstep. Your child will have 1-1 classes from our expert mentors to learn by building projects using the kit. In each class, your child will solve real world problems and build real world products that actually work! The mentor will help your child learn while building the projects for the technology and real world Problems and Solutation ."
+        "course_details": "Upon enrollment into the Robotics  Classes for kids, you will get your child's Robotics Learning kit at your doorstep,building the projects for the technology and real world Problems "
+        //Your child will have 1-1 classes from our expert mentors to learn by building projects using the kit. In each class, your child will solve real world problems and build real world products that actually work! The mentor will help your child learn while building the projects for the technology and real world Problems and Solutation .//
       },
      
       {
         "course_id": 5,
         "image_url": "	https://assets.zyrosite.com/Aq20eV79zLfpXV6b/9d7b99_dfcb8e88751c4cecb7ac677976976ec8~mv2-mP4Xa8xEPPTLEoVx.gif",
         "course_name": "Artificial intelligence",
-        "course_details": "Gain mastery in Artificial intelligence from the comfort of your home and open doors to amazing job opportunities with our certification program. Enroll in our intensive  internship, where you'll acquire knowledge in web application development and deployment . Establish a strong base for your career and real-world implementation within a supportive and collaborative setting."
+        "course_details": "Gain mastery in Artificial intelligence from the comfort of your home and open doors to amazing job opportunities with our certification program."
+        // Enroll in our intensive  internship, where you'll acquire knowledge in web application development and deployment . Establish a strong base for your career and real-world implementation within a supportive and collaborative setting.//
       },
       {
         "course_id": 6,
         "image_url": "https://previews.123rf.com/images/maxkabakov/maxkabakov1212/maxkabakov121200277/17011721-security-concept-circuit-board-with-word-cyber-security-3d-render.jpg",
         "course_name": "Cyber security",
-        "course_details": "entry-level cybersecurity jobs. Students learn about computer systems, viruses, networks, and legal issues in cybersecurity,ata security, digital forensics, ethical hacking, and cryptography,cyber networks, intrusion, threat detection, and protection,critical threat to a business' finances, productivity,Cybersecurity Engineering,areas such as digital forensics, network security, and information security management"
+        "course_details": "entry-level cybersecurity jobs. Students learn about computer systems, viruses, networks, and legal issues in cybersecurity,ata security, digital forensics. "
+        //ethical hacking, and cryptography,cyber networks, intrusion, threat detection, and protection,critical threat to a business' finances, productivity,Cybersecurity Engineering,areas such as digital forensics, network security, and information security management//
       },
       {
         "course_id": 7,
         "image_url": "https://assets.zyrosite.com/Aq20eV79zLfpXV6b/870a2f0a426751eac1fc9cefcd97be78-m7VE6evebnsJWr3w.gif",
         "course_name": "UI/UX Design",
-        "course_details": "Gain mastery in UI/UX Design from the comfort of your home and open doors to amazing job opportunities with our certification program. Enroll in our intensive  internship, where you'll acquire knowledge in web application development and deployment . Establish a strong base for your career and real-world implementation within a supportive and collaborative setting,User research,UI/UX tools,Visual design"
+        "course_details": "Gain mastery in UI/UX Design from the comfort of your home and open doors to amazing job opportunities with our certification program, UI/UX tools,Visual design"
+        //Enroll in our intensive  internship, where you'll acquire knowledge in web application development and deployment . Establish a strong base for your career and real-world implementation within a supportive and collaborative setting,User research,UI/UX tools,Visual design//
       },
       {
         "course_id": 8,
         "image_url": "https://www.shutterstock.com/image-illustration/ethical-hacking-data-breach-tracking-260nw-1182566758.jpg",
         "course_name": "Ethical Hacking",
-        "course_details": "combine theory and practice related to hacking and penetration testing,Password cracking,Social engineering,System and network scanning,Vulnerability assessment,Malicious code,Attack and penetration techniques,Malware,Operating systems,Digital forensics,Filesystems,Intrusion detection systems,Web applications, detection systems"
+        "course_details": "combine theory and practice related to hacking and penetration testing,Password cracking,Social engineering,System and network scanning."
+      //Vulnerability assessment,Malicious code,Attack and penetration techniques,Malware,Operating systems,Digital forensics,Filesystems,Intrusion detection systems,Web applications, detection systems//
       },
       {
         "course_id": 9,
         "image_url": "https://assets.zyrosite.com/Aq20eV79zLfpXV6b/creative-m5KWqZZQ0ZF2RNjg.gif",
         "course_name": "Machine Learning",
-        "course_details": "Gain mastery in Machine learning from the comfort of your home and open doors to amazing job opportunities with our certification program. Enroll in our intensive 4-week internship, where you'll acquire knowledge in web application development and deployment . Establish a strong base for your career and real-world implementation within a supportive and collaborative setting."
+        "course_details": "Gain mastery in Machine learning from the comfort of your home and open doors to amazing job opportunities with our certification program."
+        // Enroll in our intensive 4-week internship, where you'll acquire knowledge in web application development and deployment . Establish a strong base for your career and real-world implementation within a supportive and collaborative setting.//
       },
       {
         "course_id": 10,
         "image_url": "https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=328,h=243,fit=crop/Aq20eV79zLfpXV6b/java_projects_for_beginners-AGBrMZgjljfZV2EE.jpg",
         "course_name": "Java Programming",
-        "course_details": "Become a Java programming master from the convenience of your own home and unlock incredible job prospects with our certification program. Join our comprehensive 4-week internship program, where you'll learn everything from web application development to deployment using Java Build a solid foundation for your career with hands-on training and real-world application in a supportive and collaborative environment."
+        "course_details": "Become a Java programming master from the convenience of your own home and unlock incredible job prospects with our certification program. "
+        //Join our comprehensive 4-week internship program, where you'll learn everything from web application development to deployment using Java Build a solid foundation for your career with hands-on training and real-world application in a supportive and collaborative environment.//
       },
       {
         "course_id": 11,
         "image_url": "https://assets.zyrosite.com/Aq20eV79zLfpXV6b/data-science-2-mP4XPq0y3wUKory9.gif",
         "course_name": "Data Science ",
-        "course_details": "Remote Data Science Internships Are A Unique Chance To Gain Experience In The Midst Of The Virtual Workforce While Remaining Immersed In One Of The Top Organizations In The Field. Data Analysis Internships Are Some Of The Most Competitive And Popular Within The Broader Data Science Field. Big Data,Business Intelligence ,Problem-Solving ,Optimization Techniques,Data Visualisations,Deep Learning"
+        "course_details": "Remote Data Science Internships Are A Unique Chance To Gain Experience In The Midst Of The Virtual Workforce While Remaining Immersed In One Of The Top Organizations. "
+        //In The Field. Data Analysis Internships Are Some Of The Most Competitive And Popular Within The Broader Data Science Field. Big Data,Business Intelligence ,Problem-Solving ,Optimization Techniques,Data Visualisations,Deep Learning//
       },
       {
         "course_id": 12,
         "image_url": "https://thumbs.dreamstime.com/z/data-structure-concept-cell-blurred-background-d-illustration-data-structure-concept-114952426.jpg",
         "course_name": "DSA With JAVA",
-        "course_details": "This course is designed to give you a comprehensive understanding of data structures and algorithms in Java. It covers a wide range of topics, including arrays, linked lists, binary search trees, stacks, queues, hash tables, heaps, sorting algorithms, and searching algorithms. The course also includes a number of exercises and projects to help you practice what you have learned, prepare for job interviews."
+        "course_details": "This course is designed to give you a comprehensive understanding of data structures and algorithms in Java. It covers a wide range of topics,  prepare for job interviews"
+        //including arrays, linked lists, binary search trees, stacks, queues, hash tables, heaps, sorting algorithms, and searching algorithms. The course also includes a number of exercises and projects to help you practice what you have learned, prepare for job interviews.//
       }
 
   ];
